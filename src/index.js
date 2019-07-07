44a73648ebd2edc71bfdbd3cb02dd2b0ef8d6a03
Krzysztof Ryszawy <krzysztof.ryszawy@gmail.com>
2019-07-07 14:43:10 +0200

    combineReducers

--- src/index.js
+++ src/index.js
@@ -6,22 +6,61 @@
 
 // ReactDOM.render(<App />, document.getElementById('root'));
 
-import { createStore } from 'redux';
+import { createStore, combineReducers } from 'redux';
 
-const reducer = (state, action) => {
+const mathReducer = (
+  state = {
+    result: 1,
+    lastValues: []
+  },
+  action
+) => {
   switch (action.type) {
     case 'ADD':
-      state = state + action.payload;
+      state = {
+        ...state,
+        result: state.result + action.payload,
+        lastValues: [...state.lastValues, action.payload]
+      };
       break;
     case 'SUBTRACT':
-      state = state - action.payload;
+      state = {
+        ...state,
+        result: state.result - action.payload,
+        lastValues: [...state.lastValues, action.payload]
+      };
       break;
     default:
   }
   return state;
 };
 
-const store = createStore(reducer, 1);
+const userReducer = (
+  state = {
+    name: 'Max',
+    age: 27
+  },
+  action
+) => {
+  switch (action.type) {
+    case 'SET_NAME':
+      state = {
+        ...state,
+        name: action.payload
+      };
+      break;
+    case 'SET_AGE':
+      state = {
+        ...state,
+        age: action.payload
+      };
+      break;
+    default:
+  }
+  return state;
+};
+
+const store = createStore(combineReducers({ mathReducer, userReducer }));
 
 store.subscribe(() => {
   console.log('Store updated', store.getState());
